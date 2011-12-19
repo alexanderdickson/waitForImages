1f04f7f73cc3fa3c0810cf6efdee4344938abf35
Erik Zettersten <erik@zettersten.com>
2011-12-18 19:50:23 -0500

    Fixed some spacing and commenting issues. Going with the "or" operator as apposed to type checking

--- jquery.waitforimages.js
+++ jquery.waitforimages.js
@@ -28,10 +28,9 @@
     // Custom selector to find `img` elements that have a valid `src` attribute and have not already loaded.
     $.expr[':'].uncached = function(obj) {
         // Firefox will always return `true` even if the image has not been downloaded.
-	// Doing it this way works in Firefox.
+        // Doing it this way works in Firefox.
         var img = document.createElement('img');
-        // In firefox, the generated image throws a 404. Lets check first and then assign
-	img.src = img.src !== "undefined" ? img.src : obj.src;
+        img.src = img.src || obj.src // Prevents 404 request if src is undefined.
         return $(obj).is('img[src!=""]') && ! img.complete;
     };
     
